1626d58d1cd38ac92388eb72c5576d18cb7a4803
Suphaloek Khueanphet <suphaloek_khueanphet@cmu.ac.th>
2023-12-25 20:49:45 +0700

    complete_all

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,14 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+    srand(time(0));
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    int x = rand()%9;
+    cout << "You will get " << grade[x] << " in this 261102." ;
+}
